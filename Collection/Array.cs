59028532b6e3fbaa8f425f409b7996891d3f9064
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-01-30 18:21:16 +0900

    Array への要素追加/削除

---
+++ Collection/Array.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace nobnak.Collection {
+
+	public static class Array {
+		public static void Insert<T>(ref T[] list, T newone, int at) {
+			var expanded = new T[list.Length + 1];
+			System.Array.Copy(list, expanded, at);
+			System.Array.Copy(list, at, expanded, at + 1, list.Length - at);
+			expanded[at] = newone;
+			list = expanded;
+		}
+		public static void Remove<T>(ref T[] list, int at) {
+			System.Array.Copy(list, at + 1, list, at, list.Length - (at + 1));
+			System.Array.Resize(ref list, list.Length - 1);
+		}
+	}
+}
