34edd86542b1bdae487baf221032d9c0d906838c
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-03-06 20:01:26 +0900

    曲率の計算

--- Geometry/Editor/SplineEditor.cs
+++ Geometry/Editor/SplineEditor.cs
@@ -113,13 +113,30 @@ namespace nobnak.Geometry {
 				return;
 			
 			var dt = 1f / GIZMO_SMOOTH_LEVEL;
-			var startPos = CatmullSplineUtil.Position(0f, GetCP);
+			var kMin = float.MaxValue;
+			var kMax = 0f;
 			for (var i = 0; i < cps.Length; i++) {
 				var t = (float)i;
 				for (var j = 0; j < GIZMO_SMOOTH_LEVEL; j++) {
 					var k = CatmullSplineUtil.Curvature(t, spline.GetCP);
 					k = Mathf.Clamp(k, JET_K_MIN, JET_K_MAX);
-					Handles.color = EditorGUIUtility.HSVToRGB(JET_A * k + JET_B, 1f, 1f);
+					if (k < kMin)
+						kMin = k;
+					else if (kMax < k)
+						kMax = k;
+					t += dt;
+				}
+			}
+
+			var jetA = 0.66666f / (kMin - kMax);
+			var jetB = -jetA * kMax;
+			var startPos = CatmullSplineUtil.Position(0f, GetCP);
+			for (var i = 0; i < cps.Length; i++) {
+				var t = (float)i;
+				for (var j = 0; j < GIZMO_SMOOTH_LEVEL; j++) {
+					var k = CatmullSplineUtil.Curvature(t, spline.GetCP);
+					k = Mathf.Clamp(k, kMin, kMax);
+					Handles.color = EditorGUIUtility.HSVToRGB(jetA * k + jetB, 1f, 1f);
 
 					var endPos = CatmullSplineUtil.Position(t += dt, GetCP);
 					Handles.DrawLine(startPos, endPos);
